93bf34a87120b857458054e84b20f4b2503fb5c2
vsegda_vinovat <riabko97@gmail.com>
2024-02-28 14:23:47 +0200

    reg new router

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 
+
+from db.database import  engine
+from db import models
+
+from routers import user, post
+
+
+
+
 app = FastAPI(title="Social Network API")
+app.include_router(user.router)
+app.include_router(post.router)
 
 
-@app.get("/")
-def root():
-    return "Hello"
+models.Base.metadata.create_all(engine)
+
+
+
 
