96ee07014b2ca2a9dc678d2b2ebe3d3275434016
vsegda_vinovat <riabko97@gmail.com>
2024-02-29 15:45:46 +0200

    added new methods

--- backend/db/db_post.py
+++ backend/db/db_post.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm.session import Session
 from sqlalchemy import select
+from fastapi import HTTPException, status
 
 
 from db.models import Post
@@ -7,20 +8,35 @@ from routers.schemas import PostBase
 from datetime import datetime
 
 
-def create(db:Session,request:PostBase ):
+def create(db: Session, request: PostBase):
     new_post = Post(
-        **request.model_dump(exclude={'creator_id'}), 
+        **request.model_dump(exclude={"creator_id"}),
         timestamp=datetime.now(),
         user_id=request.creator_id
-        )
+    )
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
 
 
-
-
-def get_all(db:Session):
+def get_all(db: Session):
     res = db.execute(select(Post)).scalars().all()
     return res
+
+
+def delete_post(db: Session, id: int, user_id: int):
+    stmt = select(Post).where(Post.id == id)
+    post = db.execute(stmt).scalar_one_or_none()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post is not exi"
+        )
+
+    if post.user_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Only creator can delete post"
+        )
+    db.delete(post)
+    db.commit()
+    return "deleted"
