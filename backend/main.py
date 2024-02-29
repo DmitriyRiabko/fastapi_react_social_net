5289385dd4f77a7fd59efe49c8bfdb6c92f4113c
vsegda_vinovat <riabko97@gmail.com>
2024-02-29 15:53:58 +0200

    added static files and auth router

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-
-from db.database import  engine
+from db.database import engine
 from db import models
+from auth import auth
 
 from routers import user, post
-
 
 
 
 app = FastAPI(title="Social Network API")
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(auth.router)
+
 
 
 models.Base.metadata.create_all(engine)
 
 
-
-
+app.mount("/images", StaticFiles(directory="images"), name="images")
