64b7398795803a1dabb21d5ac31d4a9ace3d7ab4
vsegda_vinovat <riabko97@gmail.com>
2024-03-03 17:58:25 +0200

    added Comment model

--- backend/db/models.py
+++ backend/db/models.py
@@ -23,3 +23,19 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     
     user = relationship("User", back_populates="items")
+
+    comments = relationship('Comment', back_populates='post')
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    username = Column(String)
+    timestamp = Column(DateTime)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    
+    post = relationship("Post", back_populates="comments")
+    
+    
+    
