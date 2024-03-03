eb0b2905a543186c83d66ea58c6c3c3fc340ce6d
vsegda_vinovat <riabko97@gmail.com>
2024-03-03 18:02:45 +0200

    added function get by username

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm.session import Session
 
 from .schemas import UserDisplay, UserBase
@@ -6,17 +6,16 @@ from db.database import get_db
 from db import db_user
 
 
-router = APIRouter(
-    prefix='/user',
-    tags=['user']
-)
+router = APIRouter(prefix="/user", tags=["user"])
 
 
-
-@router.post('',response_model=UserDisplay)
-def create_user(request:UserBase, db:Session = Depends(get_db)):
+@router.post("", response_model=UserDisplay, status_code=status.HTTP_201_CREATED)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
 
+@router.get("", response_model=UserDisplay)
+def get_user_by_username(request: str, db: Session = Depends(get_db)):
+    return db_user.get_user_by_username(db, request)
 
-    
+
