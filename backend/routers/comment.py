86fdfc41635c9eb5036e4b41082a2c57fdc43d6b
vsegda_vinovat <riabko97@gmail.com>
2024-03-03 19:28:42 +0200

    added auth depends

--- backend/routers/comment.py
+++ backend/routers/comment.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm.session import Session
 
 from db.database import get_db
 from db import db_comment
+from .schemas import UserAuth
 from routers.schemas import CommentBase
+from auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix='/comment',
@@ -20,5 +22,8 @@ def comments(post_id:int,db:Session = Depends(get_db)):
 
 
 @router.post('/')
-def create(request: CommentBase, db:Session = Depends(get_db)):
+def create(request: CommentBase, 
+    db:Session = Depends(get_db),
+    current_user: UserAuth = Depends(get_current_user)
+):
     return db_comment.create(db, request)
