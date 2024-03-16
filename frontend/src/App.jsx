530fb6782f2a9370529af01498c311a5d17cbefa
vsegda_vinovat <riabko97@gmail.com>
2024-03-16 22:29:18 +0200

    added popu component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState, useEffect } from "react";
 import styles from "./App.module.scss";
-import Post from "./components/Post/Post";
-import Header from "./components/Header/Header";
+import Post from "./components/post/Post";
+import Header from "./components/header/Header";
 import { useQuery } from "@tanstack/react-query";
 import { postService } from "./services/post.service";
+import SignInWindow from "./components/signIn-window/SignInwindow";
+
 
 function App() {
   const {data:posts, isLoading, error } = useQuery({
     queryKey:['allPosts'],
     queryFn: ()=> postService.getAllPosts()
   })
+
+
   return (
     <div className={styles.app}>
+
+      <SignInWindow/>
       <div className={styles.wrapper}>
         <Header />
         <section className={styles.posts_container}>
