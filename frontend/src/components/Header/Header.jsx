3d335f06333adde1ec9b6298ea0addbcb9724045
vsegda_vinovat <riabko97@gmail.com>
2024-03-16 22:29:38 +0200

    added nav buttons

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import styles from './Header.module.scss'
+import React from "react";
+import styles from "./Header.module.scss";
+import { Tent } from "lucide-react";
+import { Button } from "@mui/material";
+import { useSignInStore } from "../../store";
 
-export default function Header () {
+export default function Header() {
+  
+  const {setOpen, isOpen} = useSignInStore()
+  
+  console.log(isOpen);
+
   return (
-    <div className={styles.header}>
-      <h1 className={styles.title}>React Social Network</h1>
-    </div>
-  )
+    <header className={styles.header}>
+      <div className={styles.title_block}>
+        <Tent size={28} color="white" />
+        <h1 className={styles.title}>React+FastApi</h1>
+      </div>
+      <div className={styles.buttons_block}>
+        <Button className={styles.signIn} onClick={()=>setOpen()} variant="outlined">Sign In</Button>
+        <Button className={styles.signUp} variant="outlined">Sign Up</Button>
+      </div>
+    </header>
+  );
 }
