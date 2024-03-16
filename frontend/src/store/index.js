88d60c47138b4506db3a7a59433a76a133a7948c
vsegda_vinovat <riabko97@gmail.com>
2024-03-16 22:28:12 +0200

    added popup state store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand'
+
+
+export const useSignInStore = create((set) => ({
+    isOpen: false,
+    setOpen:()  => set ({isOpen:true}),
+    setClose: () => set({isOpen:false})
+    
+  }))
+
+
+
+export const useSignUpStore = create((set) => ({
+    isOpen: false,
+    setOpen:()  => set ({isOpen:true}),
+    setClose: () => set({isOpen:false})
+    
+  }))
