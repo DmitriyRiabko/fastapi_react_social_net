13d43e51403747325c1c50d6f63b0f410bc04d59
vsegda_vinovat <riabko97@gmail.com>
2024-03-17 17:24:50 +0200

    added post upload components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
 import styles from "./App.module.scss";
-import Post from "./components/post/Post";
+import { Post } from "./components/post/Post";
 import Header from "./components/header/Header";
 import { useQuery } from "@tanstack/react-query";
 import { postService } from "./services/post.service";
-import SignInWindow from "./components/signIn-window/SignInwindow";
-
+import SignInWindow from "./components/signIn-window/SignInWindow";
+import SignUpWindow from "./components/signUp-window/SignUpWindow";
+import NewPostWindow from "./components/newPost-window/NewPostWindow";
 
 function App() {
-  const {data:posts, isLoading, error } = useQuery({
-    queryKey:['allPosts'],
-    queryFn: ()=> postService.getAllPosts()
-  })
-
+  const { data: posts } = useQuery({
+    queryKey: ["allPosts"],
+    queryFn: () => postService.getAllPosts(),
+  });
 
   return (
     <div className={styles.app}>
-
-      <SignInWindow/>
+      <SignUpWindow />
+      <SignInWindow />
+      <NewPostWindow />
       <div className={styles.wrapper}>
         <Header />
         <section className={styles.posts_container}>
