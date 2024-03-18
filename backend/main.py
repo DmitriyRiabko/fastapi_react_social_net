fbee41beb6e23d340fcc8aa54cb48c275a597bbb
vsegda_vinovat <riabko97@gmail.com>
2024-03-18 12:42:51 +0200

    added new startup function

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-
+import uvicorn
+import sys
 
 from db.database import engine
 from db import models
 from auth import auth
 
 from routers import user, post, comment
-
-
 
 
 app = FastAPI(title="Social Network API")
@@ -19,18 +18,18 @@ app.include_router(auth.router)
 app.include_router(comment.router)
 
 
-
 origins = [
-    'http://localhost:5173'
+    "http://localhost:5173",
+    "http://10.0.0.105:5173",
 ]
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= origins,
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -38,3 +37,10 @@ models.Base.metadata.create_all(engine)
 
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
+if __name__ == "__main__":
+    if "--global" in sys.argv:
+        uvicorn.run(app="main:app", host="0.0.0.0", port=5555, reload=True)
+    else:
+        uvicorn.run(app="main:app", port=5555, reload=True)
